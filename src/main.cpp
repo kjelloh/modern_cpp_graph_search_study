0e0e14a03b3f0f0eda90efc82ca4b809aa9e96e7
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2022-11-29 20:56:16 +0100

    Added pseudocode and namespaces for wikipedia Dijkstra implementation

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,100 @@
 #include  <sstream>
 #include <set>
 #include <vector>
+#include <limits>
 
 
 // Inspired by pseudo code in wikipedia article (https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
-/*
-"In the following pseudocode algorithm, 
-dist is an array that contains the current distances from the source to other vertices, 
-i.e. dist[u] is the current distance from the source to the vertex u. The prev array contains pointers to previous-hop nodes on the shortest path from source to the given vertex (equivalently, it is the next-hop on the path from the given vertex to the source). The code u ← vertex in Q with min dist[u], searches for the vertex u in the vertex set Q that has the least dist[u] value. Graph.Edges(u, v) returns the length of the edge joining (i.e. the distance between) the two neighbor-nodes u and v. The variable alt on line 14 is the length of the path from the root node to the neighbor node v if it were to go through u. If this path is shorter than the current shortest path recorded for v, that current path is replaced with this alt path.[7]"
-1  function Dijkstra(Graph, source):
- 2      
- 3      for each vertex v in Graph.Vertices:
- 4          dist[v] ← INFINITY
- 5          prev[v] ← UNDEFINED
- 6          add v to Q
- 7      dist[source] ← 0
- 8      
- 9      while Q is not empty:
-10          u ← vertex in Q with min dist[u]
-11          remove u from Q
-12          
-13          for each neighbor v of u still in Q:
-14              alt ← dist[u] + Graph.Edges(u, v)
-15              if alt < dist[v]:
-16                  dist[v] ← alt
-17                  prev[v] ← u
-18
-19      return dist[], prev[]
-*/  
 
-/*
-1  S ← empty sequence
-2  u ← target
-3  if prev[u] is defined or u = source:          // Do something only if the vertex is reachable
-4      while u is defined:                       // Construct the shortest path with a stack S
-5          insert u at the beginning of S        // Push the vertex onto the stack
-6          u ← prev[u]                           // Traverse from target to source
-*/
+namespace wikipedia_Dijkstra {
+  /*
+  "In the following pseudocode algorithm, 
+  dist is an array that contains the current distances from the source to other vertices, 
+  i.e. dist[u] is the current distance from the source to the vertex u. 
+  The prev array contains pointers to previous-hop nodes on the shortest path from source to the given vertex (equivalently, 
+  it is the next-hop on the path from the given vertex to the source). 
+  The code u ← vertex in Q with min dist[u], searches for the vertex u in the vertex set Q 
+  that has the least dist[u] value. Graph.Edges(u, v) returns the length of the edge joining 
+  (i.e. the distance between) the two neighbor-nodes u and v. 
+  The variable alt on line 14 is the length of the path from the root node to the neighbor node v 
+  if it were to go through u. If this path is shorter than the current shortest path recorded for v, 
+  that current path is replaced with this alt path"
+
+  1  function Dijkstra(Graph, source):
+  2      
+  3      for each vertex v in Graph.Vertices:
+  4          dist[v] ← INFINITY
+  5          prev[v] ← UNDEFINED
+  6          add v to Q
+  7      dist[source] ← 0
+  8      
+  9      while Q is not empty:
+  10          u ← vertex in Q with min dist[u]
+  11          remove u from Q
+  12          
+  13          for each neighbor v of u still in Q:
+  14              alt ← dist[u] + Graph.Edges(u, v)
+  15              if alt < dist[v]:
+  16                  dist[v] ← alt
+  17                  prev[v] ← u
+  18
+  19      return dist[], prev[]
+  */
+
+  namespace vanilla {
+  }
+
+  /*
+  Using a priority queue
+
+  A min-priority queue is an abstract data type that provides 3 basic operations: 
+    add_with_priority()
+    decrease_priority()
+    extract_min(). 
+   
+  As mentioned earlier, using such a data structure can lead to faster computing times than using a basic queue. 
+  Notably, Fibonacci heap (Fredman & Tarjan 1984) or Brodal queue offer optimal implementations for those 3 operations. 
+  As the algorithm is slightly different, we mention it here, in pseudocode as well : 
+
+  1 function Dijkstra(Graph, source):
+  2      dist[source] ← 0                           // Initialization
+  3
+  4      create vertex priority queue Q
+  5
+  6      for each vertex v in Graph.Vertices:
+  7          if v ≠ source
+  8              dist[v] ← INFINITY                 // Unknown distance from source to v
+  9              prev[v] ← UNDEFINED                // Predecessor of v
+  10
+  11         Q.add_with_priority(v, dist[v])
+  12
+  13
+  14     while Q is not empty:                      // The main loop
+  15         u ← Q.extract_min()                    // Remove and return best vertex
+  16         for each neighbor v of u:              // Go through all v neighbors of u
+  17             alt ← dist[u] + Graph.Edges(u, v)
+  18             if alt < dist[v]:
+  19                 dist[v] ← alt
+  20                 prev[v] ← u
+  21                 Q.decrease_priority(v, alt)
+  22
+  23     return dist, prev    
+  */
+
+  // Priority first search
+  namespace pfs {
+
+  }
+
+  /*
+  1  S ← empty sequence
+  2  u ← target
+  3  if prev[u] is defined or u = source:          // Do something only if the vertex is reachable
+  4      while u is defined:                       // Construct the shortest path with a stack S
+  5          insert u at the beginning of S        // Push the vertex onto the stack
+  6          u ← prev[u]                           // Traverse from target to source
+  */
+}
 
 // From youtube "Dijkstra Algorithm Coding C++ | Shortest Path | Implementation | Graphs | Greedy" (https://youtu.be/a1Z1GmKzcPs )
 namespace youtube_a1Z1GmKzcPs {
